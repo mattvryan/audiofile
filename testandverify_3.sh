13f4e44657bebf19b6b08956ca6207b019034ffb
Matt Ryan <matt@mvryan.org>
2013-04-28 17:33:41 -0600

    Refactored audiofile3 and testandverify_3.sh to work with updated code that uses ActiveMQ message queue.

--- testandverify_3.sh
+++ testandverify_3.sh
@@ -1,13 +1,3 @@
 ./audiofile3 -a ~/Music/vtest
 
-#if [ ! -f ~/.audiofile/lib.db ]; then
-#	echo "Database not created."
-#	exit 1
-#fi
-
 ./audiofile3 -r '%a/%b/%d.%n-%t-%b-%a.mp3'
-
-if [ ! -f ~/Music/vtest/Scorpions/Blackout/1.7-Arizona-Blackout-Scorpions.mp3 ]; then
-	echo "Renamed files not created."
-	exit 2
-fi
